3cb56457cd36409100d0ecf01bcffcc693675979
Zachary Latta <zchlatta@gmail.com>
2012-04-04 21:26:46 -0700

    Added character.h

---
+++ code/header/character.h
@@ -0,0 +1,15 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <string>
+
+class Character
+{
+	private:
+		string characterName;
+	public:
+		void setCharacterName();
+		string getCharacterName();
+};
+#endif
