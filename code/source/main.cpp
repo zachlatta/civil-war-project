2bba57f20d2394275f34d62cfee67865acdfaa67
Zachary Latta <zchlatta@gmail.com>
2012-04-06 12:10:26 -0700

    Added on to loadGame function. Still needs to be debugged.

--- code/source/main.cpp
+++ code/source/main.cpp
@@ -158,27 +158,97 @@ void beginNewGame()
 
 void loadGame()
 {
-	int input;
+	// Variable declarations
+	int input, loop=1;
+	string name;
 
+	// Creates a new instance of the Character class titled "character"
 	Character character;
 
-	ifstream character("character.dat");
-	if(character)
-	{
-		cout << "Save file detected!\n" << endl;
-		cout << "Would you like to load it?" << endl;
-		cout << "[1] Yes" << endl;
-		cout << "[2] No\n" << endl;
-		cout << "Your input: " << endl;
+	// Creates a new file stream "characterSave"
+	fstream characterSave.open("character.dat");
 
-		switch(input)
+	// If character.dat is not empty...
+	if(characterSave)
+	{
+		while(loop!=0)
 		{
-			case 1:
+			// Menu displayed when character.dat exists.
+			cout << "Save file detected!\n" << endl;
+			cout << "Would you like to load it?" << endl;
+			cout << "[1] Yes" << endl;
+			cout << "[2] No\n" << endl;
+			cout << "Your input: " << endl;
+
+			// Switch with input
+			switch(input)
 			{
-				character
+				// If the user inputted 1...
+				case 1:
+				{
+					// Stores the value of the first line of character.dat (character information save file) in string name
+					getLine(characterSave, name);
+
+					// Sets name method variable of Character instance "character"
+					character.setName(name);
+
+					// Closes characterSave file stream
+					characterSave.close();
+
+					// Code to resume where the user left off should be below this
+
+
+					// Stops the loop that was initiated if character.dat existed
+					loop = 0;
+
+					break;
+				}
+
+				// If the user inputted 2...
+				case 2:
+				{
+					// Tells the user that they have chosen to not load a save file
+					cout << "You have chosen to not load the save file." << endl;
+
+					// Press enter to continue...
+					pressEnterToContinue();
+
+					// Goes to main menu
+					mainMenu();
+
+					// Stops the loop that was initiated if character.dat existed
+					loop = 0;
+
+					break;
+				}
+
+				// If the user did not input 1 or 2...
+				default:
+				{
+					// Tells the user that they entered an invalid value
+					cout << "You have inputted an invalid value. Please enter either 1 or 2."
+
+					// Press enter to continue...
+					pressEnterToContinue();
+
+					break;
+				}
 			}
 		}
 
+	}
+
+	// If character.dat is empty...
+	else
+	{
+		// Tells the user that no save file has been detected
+		cout << "No save file has been detected." << endl;
+		
+		// Press enter to continue...
+		pressEnterToContinue();
+
+		// Goes to main menu
+		mainMenu();
 	}
 
 }
